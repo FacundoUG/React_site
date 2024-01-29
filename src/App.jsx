3e44dfb4463bd9e15c18c837475468cae630c045
Facundo Gasser <facundo.gasser.ft@gmail.com>
2024-01-29 00:53:34 -0300

    ReactSiteFinish

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./components/Navbar.jsx"
+import Body from "./components/Body.jsx"
+
+export default function App(){
+    return( 
+        <div>
+            <Navbar />
+            <Body />
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="/images/react-icon-small.png"/>
+            <h2>ReactFacts</h2>
+            <h3>React Course - Project 1</h3>
+        </nav>
+    )
+}
